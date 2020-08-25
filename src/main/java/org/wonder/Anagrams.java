26a29c504e4dd6a0091e8bccaee570b39bbf5ae8
NinputerWonder <ninputewonder@outlook.com>
2020-08-25 16:49:30 +0800

    4.7 test three characters, refactor, STEP 6

--- src/main/java/org/wonder/Anagrams.java
+++ src/main/java/org/wonder/Anagrams.java
@@ -1,5 +1,6 @@
 package org.wonder;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,14 +15,14 @@ public class Anagrams {
             return Arrays.asList(origin, origin.substring(1) + origin.substring(0, 1));
         }
 
-        return Arrays.asList(
-                origin.substring(0, 1) + of(dropCharacter(origin, 0)).get(0),
-                origin.substring(0, 1) + of(dropCharacter(origin, 0)).get(1),
-                origin.substring(1, 2) + of(dropCharacter(origin, 1)).get(0),
-                origin.substring(1, 2) + of(dropCharacter(origin, 1)).get(1),
-                origin.substring(2, 3) + of(dropCharacter(origin, 2)).get(0),
-                origin.substring(2, 3) + of(dropCharacter(origin, 2)).get(1)
-        );
+        List<String> result = new ArrayList<>();
+        result.add(origin.substring(0 , 1) + of(dropCharacter(origin , 0)).get(0));
+        result.add(origin.substring(0 , 1) + of(dropCharacter(origin , 0)).get(1));
+        result.add(origin.substring(1 , 2) + of(dropCharacter(origin , 1)).get(0));
+        result.add(origin.substring(1 , 2) + of(dropCharacter(origin , 1)).get(1));
+        result.add(origin.substring(2 , 3) + of(dropCharacter(origin , 2)).get(0));
+        result.add(origin.substring(2 , 3) + of(dropCharacter(origin , 2)).get(1));
+        return result;
     }
 
     static String dropCharacter(String origin, int index){
